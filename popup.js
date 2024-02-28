8fb7957aaf544a4e7d8bd318462bc81089063463
devITae <sky@mberd.com>
2024-02-28 22:35:15 +0900

    Add syntax to crawl all pages

--- popup.js
+++ popup.js
@@ -12,9 +12,84 @@ function getTabUrl(callback) {
   });
 }
 
+// Remove 'start='
 function makeURL(statusText){
-  var URL = statusText.replace(/start=(\d{1,5})&/i, ''); // Remove 'start='
+  var URL = statusText.replace(/start=(\d{1,5})&/i, ''); 
   return URL;
+}
+
+// 크롤링 한 데이터를 담는 배열
+const data = [];
+
+async function crawl(url) {
+  // 수집중인 URL
+  const decodedUrl = decodeURI(url);
+  console.log(`크롤링 ${decodedUrl} ...`);
+  // URL에서 데이터를 가져옴
+  const response = await fetch(url  , {
+      headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+          'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
+      }
+  });
+  // 응답 데이터를 문자열로 변환
+  const htmlString = await response.text();
+  // HTML 문자열을 파싱하여 DOM 객체 생성
+  const parser = new DOMParser();
+  const htmlDOM = parser.parseFromString(htmlString, 'text/html');
+
+  // 검색 결과 수
+  const num = Number(htmlDOM
+    .getElementById('gs_ab_md')
+    .textContent
+    .split('(')[0]
+    .replace(/[^0-9]/g, ''));
+  console.log(num);
+
+  for(let i=0; i<=num; i+=10){
+    const responseSearch = await fetch(url + '&start=' + i, {
+      headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+          'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
+      }
+    });
+    const htmlString = await responseSearch.text();
+    const parser = new DOMParser();
+    const htmlDOM = parser.parseFromString(htmlString, 'text/html');
+    // 검색 결과 가공
+    const contents = htmlDOM.getElementById('gs_res_ccl_mid');
+    const oneData = contents.querySelectorAll('.gs_ri');
+
+    oneData.forEach((element)=>{
+      console.log(element);
+      let title = element.querySelector('h3.gs_rt > a');
+      let url = element.querySelector('h3.gs_rt > a');
+      let cites = element.querySelector('div.gs_flb > [href*="/scholar?cites="]');
+
+      if(title != null) {
+        title = title.textContent;
+        url = url.href;
+      } else {
+        title = element.querySelector('h3.gs_rt > span')[1].textContent;
+        url = '';
+      }
+
+      if(cites != null){
+        cites = cites.textContent.replace(/[^0-9]/g, '');
+      } else{
+        cites = 0;
+      }
+      console.log(cites);
+      // 추출한 데이터를 배열에 저장
+      data.push({
+        title,
+        url,
+        cites
+      });
+      //console.log(element);
+    })
+  }
+
 }
 
 // result div에 URL 값을 뿌려주는 함수
@@ -27,51 +102,6 @@ function renderUrl(statusText) {
   } else{
     document.getElementById('result').textContent = 'This page is not Google Scholar.';
   }
-}
-
-const data = [];
-
-async function crawl(url) {
-  // 수집중인 URL
-  const decodedUrl = decodeURI(url);
-  console.log(`크롤링 ${decodedUrl} ...`);
-  // URL에서 데이터를 가져옴
-  const response = await fetch(url, {
-      headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-          'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
-      }
-  });
-  // 응답 데이터를 문자열로 변환
-  const htmlString = await response.text();
-  // HTML 문자열을 파싱하여 DOM 객체 생성
-  const parser = new DOMParser();
-  const htmlDOM = parser.parseFromString(htmlString, 'text/html');
-  const contents = htmlDOM.getElementById('gs_res_ccl_mid');
-  const oneData = contents.querySelectorAll('.gs_ri');
-
-  oneData.forEach((element)=>{
-    //console.log(element);
-    const title = element.querySelector('h3.gs_rt > a').textContent;
-    const url = element.querySelector('h3.gs_rt > a').href;
-    let cites = element.querySelector('div.gs_flb').querySelector('[href*="/scholar?cites="]');
-    if(cites != null){
-      cites = cites.textContent.replace(/[^0-9]/g, '');
-    } else{
-      cites = 0;
-    }
-
-    //cites = cites.replace(/[^0-9]/g, '');
-    //if(cites == null) cites = 0;
-    console.log(cites);
-    // 추출한 데이터를 배열에 저장
-    data.push({
-      title,
-      url,
-      cites
-    });
-    //console.log(element);
-  })
 }
 
 // 클릭 이벤트가 발생했을 경우 getTabUrl와 renderUrl 함수를 사용해
